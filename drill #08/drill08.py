dd2164b547bb42b367236f35756c19e5bbc6f01e
2021184032 @ laptop <nhc0830@naver.com>
2022-09-29 23:10:30 +0900

    drill08 혼자 수정
    캐릭터 상하좌우 조작 기능 추가
    달리는 모션 뿐이지만 오른쪽, 왼쪽 방향 바꾸는 기능 추가 구현

--- drill #08/drill08.py
+++ drill #08/drill08.py
@@ -2,40 +2,36 @@
 
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 
-
 def handle_events():
     global running
-    global idle
+    #global idle
     global dir
     global upDown
-    global idleDir
+    global rightLeft
 
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-            idle = False
+
         elif event.type == SDL_KEYDOWN:
-            idle = False
             if event.key == SDLK_RIGHT:
+                rightLeft = True
                 dir += 1
+            elif event.key == SDLK_LEFT:
+                rightLeft = False
+                dir -= 1
             elif event.key == SDLK_UP:
                 upDown += 1
-            elif event.key == SDLK_LEFT:
-                dir -= 1
             elif event.key == SDLK_DOWN:
                 upDown -= 1
             elif event.key == SDLK_ESCAPE:
                 running = False
-                idle = False
 
         elif event.type == SDL_KEYUP:
-            running = False
             if event.key == SDLK_RIGHT:
-                idleDir = 0
                 dir -= 1
-            elif event.key == SDLK_LEFT:
-                idleDir = 1
+            elif event.key ==SDLK_LEFT:
                 dir += 1
             elif event.key == SDLK_UP:
                 upDown -= 1
@@ -46,43 +42,37 @@ open_canvas(TUK_WIDTH, TUK_HEIGHT)
 tuk_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 
-running = False
-idle = True
-idleDir = 0
+running = True
+rightLeft = True
+#if running == False:
+    # idle = True
+# elif running == True:
+#     idle = False
+
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 frame = 0
 dir = 0
 upDown = 0
-# hide_cursor()
 
 while running:
     clear_canvas()
     tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    if idleDir == 0:
+
+    if rightLeft == True:
+        character.clip_draw(frame * 100, 100, 100, 100, x, y)
+
+    elif rightLeft == False:
         character.clip_draw(frame * 100, 0, 100, 100, x, y)
-    elif idleDir == 1:
-        character.clip_draw(frame * 100, 100 , 100, 100, x, y)
+
     update_canvas()
     frame = (frame + 1) % 8
     x += dir * 5
     y += upDown * 5
     handle_events()
 
-while idle:
-    clear_canvas()
-    tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    if idleDir == 0:
-        character.clip_draw(frame * 100, 301, 100, 100, x, y)
-    elif idleDir == 1:
-        character.clip_draw(frame * 100, 201, 100, 100, x, y)
-    update_canvas()
-    frame = (frame + 1) % 8
-    x += dir * 5
-    y += upDown * 5
-    handle_events()
+# while idle:
+#     clear_canvas()
+#     tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+#     character.clip_draw(frame * 100, )
 
 close_canvas()
-
-
-
-
