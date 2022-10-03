3d1014efac128add3661571b2fca073861554f15
2021184032 @ laptop <nhc0830@naver.com>
2022-10-03 22:00:54 +0900

    drill08 완성~~!!
    정지 상태와 달리는 상태일 때의 애니메이션 바뀌도록 구현 추가
    
    캐릭터가 화면 밖으로 나가지 못하도록 구현 추가

--- drill #08/drill08.py
+++ drill #08/drill08.py
@@ -1,20 +1,21 @@
 ﻿from pico2d import *
-
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 
 def handle_events():
+    global acting
     global running
     global dir
     global upDown
     global rightLeft
 
     events = get_events()
+
     for event in events:
         if event.type == SDL_QUIT:
-            running = False
+            acting = False
 
         elif event.type == SDL_KEYDOWN:
-            idle = False
+            running = True
             if event.key == SDLK_RIGHT:
                 rightLeft = True
                 dir += 1
@@ -26,10 +27,10 @@ def handle_events():
             elif event.key == SDLK_DOWN:
                 upDown -= 1
             elif event.key == SDLK_ESCAPE:
-                running = False
+                acting = False
 
         elif event.type == SDL_KEYUP:
-            idle = True
+            running = False
             if event.key == SDLK_RIGHT:
                 dir -= 1
             elif event.key ==SDLK_LEFT:
@@ -43,7 +44,8 @@ open_canvas(TUK_WIDTH, TUK_HEIGHT)
 tuk_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 
-running = True
+acting = True
+running = False
 rightLeft = True
 
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
@@ -51,38 +53,35 @@ frame = 0
 dir = 0
 upDown = 0
 
-while running:
+while acting:
     clear_canvas()
     tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-
-    if event.type == SDL_KEYDOWN:
+    if running == True:
         if rightLeft == True:
             character.clip_draw(frame * 100, 100, 100, 100, x, y)
-
         elif rightLeft == False:
             character.clip_draw(frame * 100, 0, 100, 100, x, y)
 
-    elif event.type == SDL_UP:
+    elif running == False:
         if rightLeft == True:
-             character.clip_draw(frame * 100, 300, 100, 100, x, y)
-
+            character.clip_draw(frame * 100, 300, 100, 100, x, y)
         elif rightLeft == False:
-             character.clip_draw(frame * 100, 200, 100, 100, x, y)
+            character.clip_draw(frame * 100, 200, 100, 100, x, y)
 
     update_canvas()
     frame = (frame + 1) % 8
     x += dir * 5
     y += upDown * 5
-    handle_events()
 
-# while not running:
-#     clear_canvas()
-#     tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-#
-#
-#
-#     update_canvas()
-#     frame = (frame + 1) % 8
-#     handle_events()
+    if x <= 20 :
+        x = 20
+    elif x >= 1260 :
+        x = 1260
+    if y >= 984 :
+        y = 984
+    elif y <= 40 :
+        y = 40
+
+    handle_events()
 
 close_canvas()
