23929e9e4ce80d58e495d4b5727ce01c47d61bd2
2021184032 @ laptop <nhc0830@naver.com>
2022-10-11 00:07:52 +0900

    framework
    framework만들고 이어가기
    오프닝 + 타이틀 + 게임화면 구현
    게임화면에서 esc 누르면 타이틀로, 타이틀에서 esc 누르면 종료되도록 구현
    타이틀에서 space 누르면 게임화면으로 이동하도록 구현

---
+++ framework folder/logo_state.py
@@ -0,0 +1,43 @@
+from pico2d import *
+import game_framework
+import play_state
+import title_state
+
+running = True
+image = None
+logo_time = 0.0
+
+def enter():
+    global image
+    image = load_image('tuk_credit.png')
+    pass
+
+def exit():
+    global image
+    del image
+    pass
+
+def update():
+    global logo_time
+    # global running
+    if logo_time > 1.0:
+        logo_time = 0
+        game_framework.change_state(title_state)
+      #  running = False
+    delay(0.01)
+    logo_time += 0.01
+    pass
+
+def draw():
+    clear_canvas()
+    image.draw(400, 300)
+    update_canvas()
+    pass
+
+def handle_events():
+    events = get_events()
+
+
+
+
+
---
+++ framework folder/play_state.py
@@ -0,0 +1,60 @@
+from pico2d import *
+import game_framework
+import logo_state
+import title_state
+
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(400, 30)
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = 0, 90
+        self.frame = 0
+        self.image = load_image('run_animation.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += 1
+
+    def draw(self):
+        self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.change_state(title_state)
+    delay(0.01)
+
+boy = None
+grass = None
+running = True
+
+# 게임 초기화 코드
+def enter():
+    global boy, grass, running
+    boy = Boy()
+    grass = Grass()
+    running = True
+
+# finalization code
+def exit():
+    global boy, grass
+    del boy
+    del grass
+
+def update():
+    boy.update()
+
+def draw():
+    clear_canvas()
+    grass.draw()
+    boy.draw()
+    update_canvas()
